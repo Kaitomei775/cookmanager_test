962ad3d6714216568e7544dcb2584ee4645323fb
Kaito Isono <52748145+Kaitomei775@users.noreply.github.com>
2020-07-27 17:17:54 +0900

    Update edit.php

--- cookmanager/edit.php
+++ cookmanager/edit.php
@@ -10,7 +10,7 @@ purpose : 食材編集処理
   try {
     if (empty($_GET['id'])) throw new Exception('ID不正');
     $id = (int) $_GET['id'];
-    $dbh = new PDO('mysql:host=160.16.141.77:61000;dbname=db_shokuzai;charset=utf8', $user, $pass);
+    $dbh = new PDO('mysql:host=127.0.0.1:61000;dbname=db_shokuzai;charset=utf8', $user, $pass);
     $dbh->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $sql = "SELECT * FROM shokuzai WHERE id = ?";
